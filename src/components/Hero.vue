fef3f3bc175ffabf030948708bd8745622fd9ed9
Ekawardana <082312931650as@gmail.com>
2022-11-25 10:47:08 +0700

    Membuat Menu Makanan

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -8,14 +8,14 @@
                     <div class="justify-content-center align-self-center">
                         <h2><strong class="text-primary">Favorite Foods Menu,</strong><br>In Your Gadget</h2>
                         <p>Segera pesan makanan favorit anda sebelum kehabisan!</p>
-                        <button class="btn btn-lg btn-primary">
+                        <router-link class="btn btn-lg btn-primary" to="/menu">
                             <b-icon icon="cart-plus" class="mr-2"></b-icon>Pesan
-                        </button>
+                        </router-link>
                     </div>
                 </div>
             </div>
             <div class="col-md-6">
-                <img src="../assets/image/hero.png" width="100%" />
+                <img src="assets/image/hero.png" width="100%" />
             </div>
         </div>
     </div>
@@ -24,7 +24,7 @@
     <div class="d-sm-block d-md-none">
         <div class="row">
             <div class="col-md-6 mb-3">
-                <img src="../assets/image/hero.png" width="100%" />
+                <img src="assets/image/hero.png" width="100%" />
             </div>
             <div class="col-md-6">
                 <div class="d-flex h-100">
--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,18 +1,83 @@
 <template>
 <div>
     <Navbar />
-    <h2>Daftar Menu</h2>
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col">
+                <h2>Daftar <strong class="text-primary">Menu</strong></h2>
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            <div class="col-sm-8">
+                <div class="input-group mt-3">
+                    <input type="text" v-model="search" class="form-control" placeholder="Cari Makanan.." @keyup="searchMenu" />
+
+                    <b-input-group-append>
+                        <b-button variant="primary">
+                            <b-icon icon="search"></b-icon>
+                        </b-button>
+                    </b-input-group-append>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mb-4">
+            <div class="col-md-4 mt-4" v-for="product in products" :key="product.id">
+                <CardProduct :product="product" />
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from '@/components/Navbar.vue'
+import CardProduct from '@/components/CardProduct.vue'
+import axios from 'axios'
 
 export default {
     name: 'MenuView',
     components: {
-        Navbar
+        Navbar,
+        CardProduct
+    },
+    data() {
+        return {
+            products: [],
+            search: ''
+        }
+    },
+    methods: {
+        setProduct(data) {
+            this.products = data
+        },
+
+        searchMenu() {
+            axios.get(`http://localhost:3000/products/?q=${this.search}`)
+                .then((response) =>
+                    // handle success
+                    this.setProduct(response.data)
+                )
+                .catch((error) =>
+                    // handle error
+                    console.log(error)
+                )
+        }
+
+    },
+    mounted() {
+        // Make a request for a user with a given ID
+        axios.get("http://localhost:3000/products")
+            .then((response) =>
+                // handle success
+                this.setProduct(response.data)
+            )
+            .catch((error) =>
+                // handle error
+                console.log(error)
+            )
     }
 }
 </script>
